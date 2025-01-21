0c13f5f2f1a22392178fdbd5064752841e3aa715
Shivam8552051878 <sg9967780426@gmail.com>
2025-01-21 12:06:29 +0530

    setup the project

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    """_summary_
+    This function will return list of pakages that are required for project
+
+    Args:
+        file_path (str): File path for requirement.txt
+
+    Returns:
+        List[str]: its will have list of pakage names
+    """
+    requirements = []
+    with open(file_path, 'r') as file:
+        requirements = file.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+    if HYPHEN_E_DOT in requirements:
+        requirements.remove(HYPHEN_E_DOT)
+    return requirements
+    
+setup(
+    name='mlProject',
+    version='0.0.1',
+    author='Shivam Gupta',
+    author_email='sg9967780426@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+    
+)
